ddd55baec54d2436e5b7239cbb2bf450f698d217
Allan Bezerra <allanbezerra95@gmail.com>
2023-05-20 21:51:21 -0300

    feat: melhorando visual básico

--- resources/views/import/index.blade.php
+++ resources/views/import/index.blade.php
@@ -1,3 +1,40 @@
+<style>
+    body {
+        background-color: #ffffff;
+        font-family: Arial, sans-serif;
+        text-align: center;
+        padding-top: 50px;
+    }
+
+    .logo {
+        width: 200px;
+        height: 200px;
+        margin-bottom: 30px;
+    }
+
+    form {
+        margin-bottom: 20px;
+    }
+
+    input[type="file"] {
+        display: block;
+        margin: 0 auto 10px;
+    }
+
+    button[type="submit"] {
+        background-color: #0066cc;
+        color: #ffffff;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        font-size: 16px;
+        cursor: pointer;
+        margin-top: 30px;
+    }
+</style>
+
+<img src="{{ asset('images/ai-solutions-logo.jpeg') }}" alt="Logo" class="logo">
+
 <form action="{{ route('import.store') }}" method="POST" enctype="multipart/form-data">
     @csrf
     <input type="file" name="file">
